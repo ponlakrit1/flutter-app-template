ed75ae762f52c430c249b5085328c45b6ef7d0e5
Ponlakrit kaewmoon <nctine.tech@gmail.com>
2026-01-01 23:40:30 +0700

    feat: 🎸 Add font family

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,6 +4,7 @@ import 'package:flutter_app_template/core/theme/app_color.dart';
 class AppTheme {
   static ThemeData get defaultTheme {
     return ThemeData(
+      fontFamily: 'NotoSansThais',
       primaryColor: AppColor.primary,
       scaffoldBackgroundColor: AppColor.neutral.shade100,
       primarySwatch: AppColor.primary,
